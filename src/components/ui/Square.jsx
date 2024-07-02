11cf67f585be91de0bfb829440b283bbd6fffd14
TechMrs <kemilat50@gmail.com>
2024-07-02 13:58:24 +0800

    Feat: Add cursor pointer for game boxes

--- src/components/ui/Square.jsx
+++ src/components/ui/Square.jsx
@@ -5,7 +5,7 @@ const Square = ({ value, onClick, disabled }) => {
   return (
     <div className="px-1 border border-[#503025]">
       <div
-        className={`w-20 h-20 m-2 flex justify-center items-center text-2xl font-bold rounded-md bg-[#a27754] ${
+        className={`w-20 h-20 m-2 flex justify-center items-center text-2xl font-bold rounded-md bg-[#a27754] cursor-pointer ${
           disabled ? "pointer-events-none" : ""
         } ${value === "X" ? xStyle : value === "O" ? oStyle : "bg-gray-300"}`}
         onClick={onClick}
