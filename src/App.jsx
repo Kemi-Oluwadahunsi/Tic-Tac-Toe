8867377a8687e632dbf44656706b9c5c2049f969
TechMrs <kemilat50@gmail.com>
2024-07-02 11:55:29 +0800

    Fix: Changed homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Game from "./components/ui/Game";
-import Layout from "./components/ui/layout";
 import {Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/game" element={<Game />} />
+        <Route path="/" element={<Game />} />
       </Routes>
     </>
   );
